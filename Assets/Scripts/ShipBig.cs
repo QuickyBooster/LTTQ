475853348cbf382cffe5c9da8bef91ca716f7e17
booster <quickybooster@gmail.com>
2022-10-12 16:48:05 +0700

    v2

---
+++ Assets/Scripts/ShipBig.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBig : MonoBehaviour
+{
+    Vector3 point;
+    float x;
+    float y;
+    float z;
+    private void OnMouseDrag()
+    {
+        Debug.Log("mouse is dragging it now");
+        x = Input.mousePosition.x;
+        y = Input.mousePosition.y;
+        z = (Camera.main.transform.position-gameObject.transform.position).magnitude;
+
+        point = Camera.main.ScreenToWorldPoint(new Vector3(x, y, z));
+        gameObject.transform.position = point;
+
+    }
+    private void OnMouseOver()
+    {
+    }
+}
