6f2792aad04b0971ad6dad2c98e8847cdcf43d4c
booster <quickybooster@gmail.com>
2022-11-12 12:31:54 +0700

    v18

--- Assets/Scripts/UIManagerBattle.cs
+++ Assets/Scripts/UIManagerBattle.cs
@@ -1,36 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections.LowLevel.Unsafe;
-using UnityEngine;
-using UnityEngine.UI;
+//using System.Collections;
+//using System.Collections.Generic;
+//using Unity.Collections.LowLevel.Unsafe;
+//using UnityEngine.UI;
 
-public class UIManagerBattle : MonoBehaviour
-{
-    [SerializeField] Text _textTurn;
-    [SerializeField] Controller _controller;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _controller = FindObjectOfType<Controller>();
-    }
+//public class UIManagerBattle //: MonoBehaviour
+//{
+//    //[SerializeField] Text _textTurn;
+//    //[SerializeField] Controller _controller;
+//    //// Start is called before the first frame update
+//    //void Start()
+//    //{
+//    //    _controller = FindObjectOfType<Controller>();
+//    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (_controller.isEnemyTurn())
-        {
-            this.setTextTurn("Enemy turn: ");
-        }
-        else
-        {
-            this.setTextTurn("Your turn: ");
-        }
-    }
-    public void setTextTurn(string text)
-    {
-        if (_textTurn)
-        {
-            _textTurn.text = text;
-        }
-    }
-}
+//    //// Update is called once per frame
+//    //void Update()
+//    //{
+//    //    if (_controller.isEnemyTurn())
+//    //    {
+//    //        this.setTextTurn("Enemy turn: ");
+//    //    }
+//    //    else
+//    //    {
+//    //        this.setTextTurn("Your turn: ");
+//    //    }
+//    //}
+//    //public void setTextTurn(string text)
+//    //{
+//    //    if (_textTurn)
+//    //    {
+//    //        _textTurn.text = text;
+//    //    }
+//    //}
+//}
