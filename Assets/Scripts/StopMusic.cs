b117ef5d1914a6e43037a89226d2a6685351e406
RosDeeper <hoanggg237@gmail.com>
2022-11-19 16:58:26 +0700

    fixed sound 2

--- Assets/Scripts/StopMusic.cs
+++ Assets/Scripts/StopMusic.cs
@@ -5,12 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class StopMusic : MonoBehaviour
 {
-    
-
     // Update is called once per frame
-    void Update()
+    void Start()
     {
         if (SceneManager.GetActiveScene().name == "PreBattle")
             MusicControl.instance.GetComponent<AudioSource>().Pause();
+    }
+
+    public static StopMusic instance1;
+    private void Awake()
+    {
+        if (instance1 != null)
+            Destroy(gameObject);
+        else
+        {
+            instance1 = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
     }
 }
